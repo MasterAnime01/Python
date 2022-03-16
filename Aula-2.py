4ab607a66f449409c15f003edb6f764c9b6903e5
masteranime01 <masterjoshuaprofissional@hotmail.com>
2022-03-16 12:24:31 -0300

    first commit

---
+++ Aula-2.py
@@ -0,0 +1,19 @@
+numero1 = 16
+numero2 = 2
+numero3 = 5.5
+
+resultado = numero1 ** (1/2) # pega a raiz quadrada do numero1
+
+print(resultado) # exibi a operação acima
+
+
+
+## Código Feito Anteriormente
+
+##nome = input("Digite o seu nome: ")
+##idade = input("Digite a sua idade: ")
+##altura = input("Digite a sua altura: ")
+
+##print(nome, "tem", idade, "anos", "e", "tem", altura, "de altura")
+
+##print(type(nome), type(idade), type(altura))
---
+++ Ex_Aula-2.py
@@ -0,0 +1,8 @@
+nome = input("Digite o seu nome: ")
+idade = input("Digite a sua idade: ")
+altura = input("Digite a sua altura: ")
+cpf = input("Digite o seu CPF: ")
+endereco = input("Digite o seu endereço: ")
+telefone = input("Digite o seu número de telefone: ")
+
+print(nome, "tem", idade, "anos", "tem", altura, "de altura", "seu CPF é", cpf, "o seu endereço é", endereco, "e o seu número de telefone é", telefone)
